3abbe81467a0ef22b86bb61f769d9965c85c21a7
Soumyasai29 <soum18.behera@gmail.com>
2026-01-17 14:06:06 +0530

    Update Program.cs

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -6,3 +6,11 @@ int age = int.Parse(Console.ReadLine());
 
 Console.WriteLine($"Hello {name}, you are {age} years old.");
 
+if(age<18)
+{
+    Console.WriteLine("You are a minor");
+}
+else
+{
+    Console.WriteLine("You are an adult");  
+}
