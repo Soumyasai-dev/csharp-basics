f2b4d783280973a5f6ed279cd207b15830d73f7d
Soumyasai29 <soum18.behera@gmail.com>
2026-01-17 13:53:30 +0530

    Fix Project Location and track C#

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Enter your name: "); 
+string name = Console.ReadLine();
+
+Console.Write("Enter your age: ");
+int age = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"Hello {name}, you are {age} years old.");
+
